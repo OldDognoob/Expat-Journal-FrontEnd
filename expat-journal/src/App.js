164d304f1ebf514f8cd115a238c2e674baa7b869
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-04 13:39:23 +0200

    refactoring-app-file

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+//import components
+import LoginForm from "../src/component/Entry/LoginForm";
+import SignUpForm from "../src/component/Entry/SignUpForm";
+import PrivateRoute from "./utils/PrivateRoute";
+import Nav from "./component/Navigation/Nav";
+import WelcomePage from "./component/Home/WelcomePage";
+import UserPost from "./component/Posts/UserPosts";
+import CreatePost from "./component/Posts/CreatePost";
+import UpdatePost from "../src/component/Posts/UpdatePost";
+import Browse from "../src/component/Home/Browse";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        {/* <Route exact path="/">
+            <Nav />
+            <Route exact path="/">
+              <Browse />
+            </Route>
+          </Route> */}
+        <Switch>
+          <Route exact path="/">
+            <WelcomePage />
+          </Route>
+          <Route exact path="/register">
+            <SignUpForm />
+          </Route>
+          <Route exact path="/login">
+            <LoginForm />
+          </Route>
+          <PrivateRoute exact path="/posts">
+            <Browse />
+          </PrivateRoute>
+          <PrivateRoute exact path="/createPost">
+            <CreatePost />
+          </PrivateRoute>
+          <PrivateRoute exact path="/updatePost/:id">
+            <UpdatePost />
+          </PrivateRoute>
+          <PrivateRoute exact path="/posts/:id">
+            <UserPost />
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
