414375481da99f275e063db34cb6e095d5ef24f3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-04 23:43:53 +0200

    styled-button-browse

--- expat-journal/src/component/Home/Browse.js
+++ expat-journal/src/component/Home/Browse.js
@@ -4,6 +4,40 @@ import UserPosts from "../Posts/UserPost";
 import {getPosts} from "../../State/actionCreators";
 import { Link } from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 8px 12px;
+  background-color: #24243e;
+  color: #fff;
+  width: 25%;
+  border: 1px solid #22283a;
+  padding: 1rem;
+  line-height: 1;
+  margin: 1.2rem;
+  background-color: 250ms;
+  margin-bottom: 50px;
+  margin-right: 30px;
+  border-radius: 4px;
+  font-size: 1.5rem;
+  :hover {
+    cursor: pointer;
+    background-color: white;
+    color: #2da562;
+    border: 1px solid #2da562;
+  }
+
+  label {
+    padding: 1rem;
+    color: #495057;
+  }
+  @media screen and (max-width: 500px) {
+    width: 80%;
+    margin-top: 5%;
+    margin-bottom: 5%;
+  }
+`;
+
 
 function Browse(props) {
   const{isLoadingPosts,getPosts,posts} = props
@@ -17,7 +51,7 @@ if(isLoadingPosts)return(<p>Loading...</p>)
 return (
   <div className="post-card-parent">
     <h1>Posts</h1>
-    <Link to='/createPost'><button>CreatePost</button></Link>
+    <Link to='/createPost'><StyledButton>CreatePost</StyledButton></Link>
     <Grid container justify="center" spacing={4} className="post-cards">
       {posts.map(post => (
         <Grid key={post.id} item>
